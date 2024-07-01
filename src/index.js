80b1bbdd25c8384dd064a7f83b638e6ac17a1bd7
Venom <adityagupta9825@gmail.com>
2024-07-01 18:28:02 +0530

    utill code desc

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import connectDB from "./db/index.js";
 
 dotenv.config({path: "./env"});
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8080 ,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
 
 
 
---
+++ src/utils/ApiResponce.js
@@ -0,0 +1,10 @@
+class ApiResponce {
+    constructor(staausCode, message = "success",data){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = success;
+        this.success = staausCode < 400;
+        
+    }
+}
